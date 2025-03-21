e9138e9b670a6efa7c5ca73005d02f3088dcef78
Brenden Dougherty <bdougherty89@yahoo.com>
2025-03-20 22:36:54 -0700

    Added functionality to create new book button and re did design on book divs

--- javascript.js
+++ javascript.js
@@ -4,16 +4,68 @@ let counter = 0;
 
 const myLibrary = [];
 
-function Book(title, author, pages, read) {
+function Book(title, author, pages, read, id, created) {
   this.title = title;
   this.author = author;
   this.pages = pages;
   this.read = read;
+  this.id = id;
+  this.created = created;
+}
+function addBookToUi() {
+  for (let i = 0; i < myLibrary.length; i++) {
+    console.log(myLibrary[i].created);
+    if (myLibrary[i].created == 0) {
+      const newBookContainer = document.createElement("div");
+      newBookContainer.className = "book-container";
+      const title = document.createElement("h2");
+      title.textContent = "Title:";
+      const titleName = document.createElement("p");
+      titleName.textContent = myLibrary[i].title;
+      const author = document.createElement("h2");
+      author.textContent = "Author:";
+      const authorName = document.createElement("p");
+      authorName.textContent = myLibrary[i].author;
+      const pages = document.createElement("h2");
+      pages.textContent = "Pages:";
+      const pagesCount = document.createElement("p");
+      pagesCount.textContent = myLibrary[i].pages;
+      const read = document.createElement("h2");
+      read.textContent = "Read:";
+      const readStatus = document.createElement("p");
+      readStatus.textContent = myLibrary[i].read;
+      newBookContainer.append(
+        title,
+        titleName,
+        author,
+        authorName,
+        pages,
+        pagesCount,
+        read,
+        readStatus
+      );
+
+      const buttonDiv = document.createElement("div");
+      buttonDiv.className = "button-div";
+      const deleteButton = document.createElement("button");
+      deleteButton.className = "delete-button";
+      deleteButton.textContent = "Delete";
+      const readButton = document.createElement("button");
+      readButton.className = "read-button";
+      readButton.textContent = "Read";
+      newBookContainer.appendChild(buttonDiv);
+      buttonDiv.append(deleteButton, readButton);
+      console.log(myLibrary[i]);
+      myLibrary[i].created = 1;
+      screenContainer.append(newBookContainer);
+    } else {
+    }
+  }
 }
 
 addBookButton.addEventListener("click", createBookInputField);
 
-function createBookInputField(event) {
+function createBookInputField() {
   if (counter == 0) {
     const form = document.createElement("form");
     form.className = "add-book-form";
@@ -71,8 +123,9 @@ function createBookInputField(event) {
     screenContainer.appendChild(addBookContainer);
     addBookContainer.appendChild(form);
     form.appendChild(submitButton);
-    event.preventDefault();
-    submitButton.addEventListener("click", function () {
+    submitButton.addEventListener("click", addBookToArray);
+    function addBookToArray(event) {
+      event.preventDefault();
       const titleInput = document.querySelector(".title-input");
       const authorInput = document.querySelector(".author-input");
       const pagesInput = document.querySelector(".pages-input");
@@ -82,18 +135,19 @@ function createBookInputField(event) {
         titleInput.value,
         authorInput.value,
         pagesInput.value,
-        readInput.value
+        readInput.checked,
+        self.crypto.randomUUID(),
+        0
       );
       myLibrary.push(newBook);
       console.log(myLibrary);
       counter = 0;
-
       addBookContainer.remove();
-    });
+      addBookToUi();
+    }
   } else {
     return null;
   }
+
   counter += 1;
 }
-
-function addBookToLibrary() {}
